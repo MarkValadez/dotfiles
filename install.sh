b0819ac494cd6c7b9381f1eac403a395c2da00d4
Mark Valadez <markvaladez@me.com>
2025-04-19 20:46:14 -0500

    add ssh dir if not found to install

--- install.sh
+++ install.sh
@@ -55,3 +55,7 @@ zsh_path=$(command -v zsh)
 if ! grep -Fxq "$zsh_path" /etc/shells; then
     echo "$zsh_path" | sudo tee -a /etc/shells
 fi
+
+if [ ! -d "$HOME/.ssh/" ]; then mkdir "$HOME/.ssh/" ; fi # added by Nix installer
+
+zsh
