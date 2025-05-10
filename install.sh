6ab69f48ec175182a17035e2c8635e8cf235952e
Mark Valadez <30632710+MarkValadez@users.noreply.github.com>
2025-05-09 19:29:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ chsh -s "$(which zsh)"
 [[ -f "./link-downloads.zsh" ]] && . "./link-downloads.sh"
 
 # Install Homebrew
-NONINTERACTIVE=1
+export NONINTERACTIVE=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 zsh -c "$(brew install jandedobbeleer/oh-my-posh/oh-my-posh)"
 
