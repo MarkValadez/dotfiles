1eca49ab3319757cb6f9058782079d80ff85b12a
Mark Valadez <30632710+MarkValadez@users.noreply.github.com>
2025-07-17 15:24:00 -0500

    Add astronomer completions plugin

---
+++ plugins/astronomer/astronomer.plugin.zsh
@@ -0,0 +1,15 @@
+# Autocompletion for Astronomer’s astro CLI
+if (( ! $+commands[astro] )); then
+  return
+fi
+
+# If the completion file doesn’t exist yet, generate it and bind it to `astro`.
+# Otherwise, compinit will have already loaded it.
+if [[ ! -f "${ZSH_CACHE_DIR:-$HOME/.cache}/completions/_astro" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _astro
+  _comps[astro]=_astro
+fi
+
+# (Re)generate completion spec in the cache dir in case astro was upgraded.
+astro completion zsh >| "${ZSH_CACHE_DIR:-$HOME/.cache}/completions/_astro"
