ec150343f7847ec24c86638188f7d865ddb40e5b
Mark Valadez <30632710+MarkValadez@users.noreply.github.com>
2025-07-29 20:59:35 -0500

    Update astronomer.plugin.zsh

--- plugins/astronomer/astronomer.plugin.zsh
+++ plugins/astronomer/astronomer.plugin.zsh
@@ -5,11 +5,11 @@ fi
 
 # If the completion file doesn’t exist yet, generate it and bind it to `astro`.
 # Otherwise, compinit will have already loaded it.
-if [[ ! -f "${ZSH_CACHE_DIR:-$HOME/.cache}/completions/_astro" ]]; then
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_astro" ]]; then
   typeset -g -A _comps
   autoload -Uz _astro
   _comps[astro]=_astro
 fi
 
 # (Re)generate completion spec in the cache dir in case astro was upgraded.
-astro completion zsh >| "${ZSH_CACHE_DIR:-$HOME/.cache}/completions/_astro"
+astro completion zsh > "$ZSH_CACHE_DIR/completions/_astro"
